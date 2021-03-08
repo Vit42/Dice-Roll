ce3942b9d63a0c6f470eb7d5af9945e466a2e6b2
Vit 42 <v.v.safronov@gmail.com>
2021-03-08 18:05:08 +0100

    initial commit

--- Dice Roll/ViewController.swift
+++ Dice Roll/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    @IBAction func rollPressed(_ sender: UIButton) {
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ] //creates an constant = array of dices to choose from
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)] //returns a random number between 0 and 5 choosing images from diceArray for the left dice
+        diceImageView2.image = diceArray[Int.random(in: 0...5)] //returns a random number between 0 and 5 choosing images from diceArray for the right dice
+        
     }
-
-
 }
-
