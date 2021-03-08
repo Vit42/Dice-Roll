a36b35cbab637f66b16fee1bba603f3b3a7b22f3
Vit 42 <v.v.safronov@gmail.com>
2021-03-08 18:01:39 +0100

    Initial Commit

---
+++ Dice Roll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice Roll
+//
+//  Created by Vitalii Safronov on 2021. 03. 08..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
